ceb5273b41306a9c9f40b157a13992945b5f5c52
Julio Cesar Toribio Mateo <UIDEXT0383@ccr.co.cr>
2024-10-29 12:47:41 -0500

    Inicialización del proyecto Chakra Analysis con Flask y SVG

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hola, Vercel!"})
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
